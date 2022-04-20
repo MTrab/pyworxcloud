d7424a776c42f79434ef24b7b51c6468a65dbc3f
MTrab <morten@trab.dk>
2022-04-20 10:04:47 +0200

    Migrate to requirements.txt file

--- setup.py
+++ setup.py
@@ -2,23 +2,21 @@
 
 import setuptools
 
-from pyworxcloud import __version__
-
-# requirements = ['paho-mqtt==1.6.1',
-# 'pyOpenSSL==22.0.0',
-# 'ratelimit==2.2.1']
-
-with open("requirements.txt") as reqs_file:
-    requirements = reqs_file.readlines()
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setuptools.setup(
     name="pyworxcloud",
-    version=__version__,
+    version="1.4.14,
     description="Worx Landroid API library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Morten Trab",
     author_email="morten@trab.dk",
     license="MIT",
     url="https://github.com/mtrab/pyworxcloud",
     packages=setuptools.find_packages(),
-    install_requires=requirements,
+    project_urls={
+        "Bug Tracker": "https://github.com/mtrab/pyworxcloud/issues",
+    },
 )
