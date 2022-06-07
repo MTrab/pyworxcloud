11891f0e75672f2f7a6ef36a4005b05fb1e834e1
Malene <malene@trab.dk>
2022-06-07 10:33:41 +0200

    Update version attribute

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pyworxcloud",
-    version="2.0.2",
+    version="2.0.3",
     description="Landroid cloud API library",
     long_description=long_description,
     long_description_content_type="text/markdown",
