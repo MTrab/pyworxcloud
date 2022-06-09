6bf70bbd12c3854094b7645384063d03d86ab8cd
Malene <malene@trab.dk>
2022-06-09 14:10:46 +0200

    Update test

--- test.py
+++ test.py
@@ -1,35 +1,29 @@
-import inspect
 from os import environ
 from pprint import pprint
 
 from pyworxcloud import WorxCloud
-from pyworxcloud.clouds import CloudType
 
 EMAIL = environ["EMAIL"]
 PASS = environ["PASSWORD"]
 TYPE = "worx"
 
 if __name__ == "__main__":
-    # cloud = WorxCloud(EMAIL, PASS, TYPE)
+    cloud = WorxCloud(EMAIL, PASS, TYPE)
 
-    # # Initialize connection
-    # auth = cloud.authenticate()
+    # Initialize connection
+    auth = cloud.authenticate()
 
-    # if not auth:
-    #     # If invalid credentials are used, or something happend during
-    #     # authorize, then exit
-    #     exit(0)
+    if not auth:
+        # If invalid credentials are used, or something happend during
+        # authorize, then exit
+        exit(0)
 
-    # # Connect to device with index 0 (devices are enumerated 0, 1, 2 ...) and do
-    # # not verify SSL (False)
-    # cloud.connect(0, False)
+    # Connect to device with index 0 (devices are enumerated 0, 1, 2 ...) and do
+    # not verify SSL (False)
+    cloud.connect(0, False)
 
-    # # Read latest states received from the device
-    # cloud.update()
+    # Read latest states received from the device
+    cloud.update()
 
-    # # Print all vars and attributes of the cloud object
-    # pprint(vars(cloud))
-
-    for name, cloud in inspect.getmembers(CloudType):
-        if inspect.isclass(cloud) and not "__" in name:
-            print(name.capitalize())
+    # Print all vars and attributes of the cloud object
+    pprint(vars(cloud))
