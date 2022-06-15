87b1b7a91fd789f338510ba7e7374e626b1d2b10
Malene <malene@trab.dk>
2022-06-15 15:24:11 +0200

    Add utils init

---
+++ pyworxcloud/utils/__init__.py
@@ -0,0 +1,27 @@
+"""Utils."""
+
+from .battery import (
+    Battery,
+)
+from .blades import (
+    Blades,
+)
+from .capability import (
+    Capability,
+    DeviceCapability,
+)
+from .location import (
+    Location,
+)
+from .orientation import (
+    Orientation,
+)
+
+__all__ = [
+    "Battery",
+    "Blades",
+    "Capability",
+    "DeviceCapability",
+    "Location",
+    "Orientation",
+]
