b08afb6f3f8c605a64f4084c87f705be6ea9e074
Malene <malene@trab.dk>
2022-06-21 12:31:17 +0200

    Fixing missing blades processing

--- pyworxcloud/utils/blades.py
+++ pyworxcloud/utils/blades.py
@@ -8,32 +8,35 @@ class Blades(LDict):
 
     def __init__(
         self,
-        indata=None,
+        data=None,
     ) -> None:
         """Initialize blade object."""
         super().__init__()
-        if not indata:
+        from ..helpers import string_to_time
+
+        if isinstance(data, type(None)):
+            print(type(data))
             return
 
-        if hasattr(indata, "blade_work_time"):
+        if hasattr(data, "blade_work_time"):
             # Total time with blades on in minutes
-            self["total_on"] = int(indata.blade_work_time)
+            self["total_on"] = int(data.blade_work_time)
         else:
             self["total_on"] = None
 
-        if hasattr(indata, "blade_work_time_reset"):
+        if hasattr(data, "blade_work_time_reset"):
             # Blade time reset at minutes
-            self["reset_at"] = int(indata.blade_work_time_reset)
+            self["reset_at"] = int(data.blade_work_time_reset)
         else:
             self["reset_at"] = None
 
-        if hasattr(indata, "blade_work_time_reset_at"):
+        if hasattr(data, "blade_work_time_reset_at"):
             # Blade time reset time and date
-            self["reset_time"] = indata.blade_work_time_reset_at
+            self["reset_time"] = string_to_time(data.blade_work_time_reset_at)
         else:
             self["reset_time"] = None
 
-        # Calculate blade indata since reset, if possible
+        # Calculate blade data since reset, if possible
         if self["reset_at"] and self["total_on"]:
             # Blade time since last reset
             self["current_on"] = int(self["total_on"] - self["reset_at"])
