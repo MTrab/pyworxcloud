06b683a108d611de837d5a4fcf9c9f74d1587c5d
Malene <malene@trab.dk>
2022-06-22 11:32:22 +0200

    Add logger helper

--- pyworxcloud/helpers/__init__.py
+++ pyworxcloud/helpers/__init__.py
@@ -1,8 +1,11 @@
 """Helpers classes."""
 
+from .logger import (
+    get_logger,
+)
 from .time_format import (
     convert_to_time,
     string_to_time,
 )
 
-__all__ = [convert_to_time, string_to_time]
+__all__ = [convert_to_time, get_logger, string_to_time]
