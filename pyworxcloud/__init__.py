84189c55dda2e9b0b6502243179532cd178f8d71
Malene <malene@trab.dk>
2022-07-18 17:00:50 +0200

    Raising ratelimit for accounts with more than 3 devices

--- pyworxcloud/__init__.py
+++ pyworxcloud/__init__.py
@@ -13,6 +13,8 @@ import OpenSSL.crypto
 import paho.mqtt.client as mqtt
 from paho.mqtt.client import connack_string, error_string
 
+from pyworxcloud.utils.mqtt import PUBLISH_CALLS_LIMIT, PUBLISH_LIMIT_PERIOD
+
 from .api import LandroidCloudAPI
 from .clouds import CloudType
 from .day_map import DAY_MAP
@@ -232,6 +234,14 @@ class WorxCloud(dict):
             self._worx_mqtt_client_id,
             protocol=mqtt.MQTTv311,
         )
+        
+        if len(self.devices) > 3:
+            # Raising the ratelimit as we have more devices to handle
+            # !! This has potential to get you banned !!
+            self.mqtt.set_ratelimit(
+                PUBLISH_CALLS_LIMIT * len(self.devices), PUBLISH_LIMIT_PERIOD
+            )
+
         self._log.debug("Done setting up MQTT handler, setting MQTT config.")
 
         self.mqtt.endpoint = self._endpoint
