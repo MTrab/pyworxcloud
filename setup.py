edf87661048fe6378241c0ba166f9e8bcc64c6c5
Malene Trab <malene@trab.dk>
2022-11-20 00:28:22 +0100

    Setup file update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys
 import setuptools
 
 if sys.version_info < (3, 9, 0):
-    sys.exit("The pyWorxcloud module requires Python 3.10.0 or later")
+    sys.exit("The pyWorxcloud module requires Python 3.9.0 or later")
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
@@ -15,8 +15,8 @@ setuptools.setup(
     description="Landroid cloud API library",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    author="Morten Trab",
-    author_email="morten@trab.dk",
+    author="Malene Trab",
+    author_email="malene@trab.dk",
     license="MIT",
     url="https://github.com/mtrab/pyworxcloud",
     packages=setuptools.find_packages(),
