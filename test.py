3a70918b10561e274fcd4e264b0919c383e2267d
Malene Trab <malene@trab.dk>
2023-03-31 20:53:57 +0200

    Updating the basic test-file

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
-import asyncio
+"""Basic test file."""
 import datetime
-import time
 from os import environ
 
 from pyworxcloud import WorxCloud
@@ -16,27 +15,9 @@ print("\033c", end="")
 
 # Initialize the class
 cloud = WorxCloud(EMAIL, PASS, TYPE)
+cloud.authenticate()
 cloud.connect()
 
-cloud.update(cloud.mowers[0]["serial_number"])
+print(vars(cloud))
 
-has_data = False
-while 1:
-    print("\033c", end="")
-    for mower in cloud.mowers:
-        if not mower["has_data"]:
-            print(f"{mower['name']} has not reported any state")
-        else:
-            print(
-                f"{mower['name']} was last updated at {mower['cfg']['tm']} {mower['cfg']['dt']}"
-            )
-
-# while not cloud.mowers[0]['has_data']:
-#     print(".",end="")
-
-# print("")
-# print(
-#     f"{cloud.mowers[0]['name']} was last updated at {cloud.mowers[0]['cfg']['tm']} {cloud.mowers[0]['cfg']['dt']}"
-# )
-time.sleep(10)
 cloud.disconnect()
