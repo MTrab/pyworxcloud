MTrab/pyworxcloud

commit 9a88283d6854389d360d7dbb870450501b634fa8

Fix invalid zone detection

pyworxcloud/__init__.py
@@ -912,7 +912,10 @@ class WorxCloud(dict):
             if not isinstance(zone, int):
                 zone = int(zone)
 
-            if device.zone["starting_point"][zone] == 0:
+            if (
+                zone not in device.zone["starting_point"]
+                or device.zone["starting_point"][zone] == 0
+            ):
                 raise ZoneNotDefined("Cannot request this zone as it is not defined.")
 
             if not zone in device.zone["indicies"]:

commit 87336b8f005593c1bb5b23a9c4253710ceae2b53

Fixed broken zone existance check
The bug was introduced by commit 9a88283.

This fixes https://github.com/MTrab/landroid_cloud/issues/585

pyworxcloud/__init__.py
@@ -918,14 +918,14 @@ class WorxCloud(dict):
                 zone = int(zone)
 
             if (
-                zone not in device.zone["starting_point"]
+                zone >= len(device.zone["starting_point"])
                 or device.zone["starting_point"][zone] == 0
             ):
-                raise ZoneNotDefined("Cannot request this zone as it is not defined.")
+                raise ZoneNotDefined("Cannot request zone {} as it is not defined.".format(zone))
 
             if not zone in device.zone["indicies"]:
                 raise ZoneNoProbability(
-                    "Cannot request this zone as it has no probability set."
+                    "Cannot request zone {} as it has no probability set.".format(zone)
                 )
 
             current_zones = device.zone["indicies"]