a251bac6264830ca345e84ced820c7dd315d2e13
Malene Trab <malene@trab.dk>
2025-04-03 10:14:15 +0200

    Enhance MQTT connect and reconnect

--- test.py
+++ test.py
@@ -12,19 +12,21 @@ TYPE = environ["TYPE"]
 # Clear the screen for better visibility when debugging
 print("\033c", end="")
 
-# Initialize the class
-cloud = WorxCloud(EMAIL, PASS, TYPE, tz="Europe/Copenhagen")
-cloud.authenticate()
-cloud.connect()
+iter = 1
+max = 1
 
-# print(vars(cloud))
+while iter <= max:
+    # Initialize the class
+    cloud = WorxCloud(EMAIL, PASS, TYPE, tz="Europe/Copenhagen")
+    cloud.authenticate()
+    cloud.connect()
+    for _, device in cloud.devices.items():
+        cloud.update(device.serial_number)
+        # pprint(vars(device))
+        print(f"{device.name} online: {device.online}")
 
-for _, device in cloud.devices.items():
-    cloud.update(device.serial_number)
-    # pprint(vars(device))
-    print(f"{device.name} online: {device.online}")
+        # cloud.set_offlimits(device.serial_number, False)
+        # cloud.set_offlimits_shortcut(device.serial_number, True)
 
-    # cloud.set_offlimits(device.serial_number, False)
-    # cloud.set_offlimits_shortcut(device.serial_number, True)
-
-cloud.disconnect()
+    cloud.disconnect()
+    iter += 1
