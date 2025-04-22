f483ff9d4334b075d8b221132860aefa234fb83a
Malene Trab <malene@trab.dk>
2025-04-22 15:07:44 +0200

    Reset API scheduler when receiving MQTT update

--- pyworxcloud/__init__.py
+++ pyworxcloud/__init__.py
@@ -354,6 +354,8 @@ class WorxCloud(dict):
         except json.decoder.JSONDecodeError:
             logger.debug("Malformed MQTT message received")
 
+        self._schedule_api_refresh()
+
     def _on_api_update(self, data):  # , topic, payload, dup, qos, retain, **kwargs):
         """Triggered when API has been updated."""
         logger = self._log.getChild("API_update")
@@ -396,6 +398,11 @@ class WorxCloud(dict):
     def _schedule_api_refresh(self) -> None:
         """Schedule the API refresh."""
         logger = self._log.getChild("API_Refresh_Scheduler")
+
+        try:
+            self._timers["api"].cancel()
+        except KeyError:
+            pass
 
         refresh_secs = API_REFRESH_TIME * 60
         timezone = (
