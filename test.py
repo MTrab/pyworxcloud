91695e304512349142ab4ebca3229875e38766da
Malene Trab <malene@trab.dk>
2025-04-26 15:21:18 +0200

    Update test file

--- test.py
+++ test.py
@@ -25,10 +25,10 @@ while iter <= max:
     cloud.authenticate()
     cloud.connect()
 
-    for _, device in cloud.devices.items():
+    # for _, device in cloud.devices.items():
         # cloud.update(device.serial_number)
         # pprint(vars(device))
-        print(f"{device.name} online: {device.online}")
+        # print(f"{device.name} online: {device.online}")
 
         # cloud.set_offlimits(device.serial_number, False)
         # cloud.set_offlimits_shortcut(device.serial_number, True)
